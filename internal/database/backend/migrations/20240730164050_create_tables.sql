5da4848205d6d54775c8228dd064685bc3a7860a
Denis Lebedev <3d.lebedzeu@gmail.com>
2024-07-30 22:01:37 +0300

    create dbscheme, migrations and test them

---
+++ internal/database/backend/migrations/20240730164050_create_tables.sql
@@ -0,0 +1,33 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE torrents
+(
+  user_id        BIGINT    NOT NULL,
+  torrent_link   TEXT      NOT NULL,
+  telegram_link  TEXT      NULL     DEFAULT NULL,
+  time_added     TIMESTAMP NOT NULL,
+  time_started   TIMESTAMP NULL     DEFAULT NULL,
+  time_finished  TIMESTAMP NULL     DEFAULT NULL,
+  error          TEXT      NULL     DEFAULT NULL
+);
+
+CREATE TABLE users
+(
+  id            BIGINT NOT NULL,
+  telegram_name TEXT   NOT NULL,
+  priority      INT    NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE torrents
+  ADD CONSTRAINT FK_users_TO_torrents
+    FOREIGN KEY (user_id)
+    REFERENCES users (id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE torrents;
+
+DROP TABLE users;
+-- +goose StatementEnd
