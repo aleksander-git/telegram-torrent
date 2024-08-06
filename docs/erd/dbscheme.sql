fbccd9dbbf23a7510d34797d8ccb51d866ea8716
Denis Lebedev <3d.lebedzeu@gmail.com>
2024-08-06 11:13:41 +0300

    modify db schema and migrations

--- docs/erd/dbscheme.sql
+++ docs/erd/dbscheme.sql
@@ -1,23 +1,39 @@
+CREATE TABLE torrent_x_user
+(
+  torrent_id BIGINT  NOT NULL,
+  user_id    BIGINT  NOT NULL,
+  sent       BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (torrent_id, user_id)
+);
+
 CREATE TABLE torrents
 (
-  user_id        BIGINT    NOT NULL,
-  torrent_link   TEXT      NOT NULL,
-  telegram_link  TEXT      NULL     DEFAULT NULL,
-  time_added     TIMESTAMP NOT NULL,
-  time_started   TIMESTAMP NULL     DEFAULT NULL,
-  time_finished  TIMESTAMP NULL     DEFAULT NULL,
-  error          TEXT      NULL     DEFAULT NULL
+  id            BIGINT    NOT NULL GENERATED ALWAYS AS IDENTITY,
+  message_id    BIGINT    DEFAULT NULL,
+  torrent_link  TEXT      NOT NULL UNIQUE,
+  name          TEXT      DEFAULT NULL,
+  size          BIGINT    DEFAULT NULL,
+  time_added    TIMESTAMP NOT NULL,
+  time_started  TIMESTAMP DEFAULT NULL,
+  time_finished TIMESTAMP DEFAULT NULL,
+  error         TEXT      DEFAULT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE users
 (
-  id            BIGINT NOT NULL,
-  telegram_name TEXT   NOT NULL,
-  priority      INT    NOT NULL DEFAULT 0,
+  id        BIGINT NOT NULL,
+  chat_id   BIGINT NOT NULL,
+  priority  INT    NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
 
-ALTER TABLE torrents
-  ADD CONSTRAINT FK_users_TO_torrents
+ALTER TABLE torrent_x_user
+  ADD CONSTRAINT FK_torrents_TO_torrent_x_user
+    FOREIGN KEY (torrent_id)
+    REFERENCES torrents (id);
+
+ALTER TABLE torrent_x_user
+  ADD CONSTRAINT FK_users_TO_torrent_x_user
     FOREIGN KEY (user_id)
     REFERENCES users (id);
